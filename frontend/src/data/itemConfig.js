a6f6226503a4ab38aba83142b27a597945debb04
Satel14 <ostaplvov@gmail.com>
2022-09-27 20:03:54 +0300

    added case opening animation

---
+++ frontend/src/data/itemConfig.js
@@ -0,0 +1,9 @@
+const rariestColors = {
+    "FACTORY_NEW": "#5151f3",
+    "MINIMAL_WEAR": "#5151f3",
+    "FIELD_TESTED": "#5b2421",
+    "WELL_WORN": "#655e32",
+    "BATTLE_SCARRED": "#b1015a",
+}
+
+export default rariestColors
---
+++ frontend/src/data/testOpenCase.js
@@ -0,0 +1,25 @@
+const openCase = {
+    resultWithItem: [
+        { id: 4, name: "Bloodsport", type: "assault_rifles", rare: "MINIMAL_WEAR" },
+        { id: 1, name: "Chameleon", type: "assault_rifles", rare: "MINIMAL_WEAR" },
+        { id: 13, name: "Fade", type: "sniper_rifles", rare: "MINIMAL_WEAR" },
+        { id: 3, name: "Hyper Beast", type: "assault_rifles", rare: "MINIMAL_WEAR" },
+        { id: 2, name: "Ocean Drive", type: "pistols", rare: "FACTORY_NEW" },
+        { id: 7, name: "Printstream", type: "pistols", rare: "WELL_WORN" },
+        { id: 5, name: "Hyper Beast", type: "pistols", rare: "MINIMAL_WEAR" },
+        { id: 4, name: "Bloodsport", type: "assault_rifles", rare: "MINIMAL_WEAR" },
+        { id: 1, name: "Chameleon", type: "assault_rifles", rare: "MINIMAL_WEAR" },
+        { id: 13, name: "Fade", type: "sniper_rifles", rare: "MINIMAL_WEAR" },
+        { id: 3, name: "Hyper Beast", type: "assault_rifles", rare: "MINIMAL_WEAR" },
+        { id: 7, name: "Printstream", type: "pistols", rare: "WELL_WORN" },
+        { id: 5, name: "Hyper Beast", type: "pistols", rare: "MINIMAL_WEAR" },
+        { id: 5, name: "Hyper Beast", type: "pistols", rare: "MINIMAL_WEAR" },
+        { id: 4, name: "Bloodsport", type: "assault_rifles", rare: "MINIMAL_WEAR" },
+        
+    ],
+    winner: {
+        winIndex: 54,
+        item: { id: 4, name: "Bloodsport", type: "assault_rifles", rare: "MINIMAL_WEAR" }
+    }
+}
+export default openCase
