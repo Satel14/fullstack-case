ce246a0e847fc69a3c6d9c6230ead537f7758d82
Satel14 <ostaplvov@gmail.com>
2026-09-22 17:02:58 +0300

    feat(admin): editable case table

--- frontend/src/pages/Admin/index.jsx
+++ frontend/src/pages/Admin/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Tabs } from 'antd';
 import { useTranslation } from 'react-i18next';
+import CasesTab from './CasesTab';
 
 const { TabPane } = Tabs;
 
@@ -11,7 +12,9 @@ const Admin = () => {
         <div className="adminpage">
             <h1>{t('admin.title')}</h1>
             <Tabs defaultActiveKey="cases">
-                <TabPane tab={t('admin.tabs.cases')} key="cases" />
+                <TabPane tab={t('admin.tabs.cases')} key="cases">
+                    <CasesTab />
+                </TabPane>
                 <TabPane tab={t('admin.tabs.users')} key="users" />
                 <TabPane tab={t('admin.tabs.journal')} key="journal" />
             </Tabs>
